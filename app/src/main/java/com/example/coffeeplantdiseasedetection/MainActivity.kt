ca0dd74fb4f846279ae6de492e1e11ea660de0be
ameshmjayaweera <ameshmbjyw97@gmail.com>
2023-04-18 21:30:05 +0530

    implement welcome screen

--- app/src/main/java/com/example/coffeeplantdiseasedetection/MainActivity.kt
+++ app/src/main/java/com/example/coffeeplantdiseasedetection/MainActivity.kt
@@ -2,18 +2,33 @@ package com.example.coffeeplantdiseasedetection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container)
-                as NavHostFragment
-        val navController = navHostFragment.navController
-        val navInflater = navController.navInflater
-        val graph = navInflater.inflate(R.navigation.nav_graph)
-        navController.graph = graph
+        // Get the NavHostFragment from the layout
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        // Get the NavController from the NavHostFragment
+        navController = navHostFragment.navController
+
+        // Set up the ActionBar
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+
+        supportActionBar?.hide() // if using ActionBar
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
