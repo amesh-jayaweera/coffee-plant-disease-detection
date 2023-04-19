f3ef5376893bb6babbb8174aa824c471ae7ecb30
ameshmjayaweera <ameshmbjyw97@gmail.com>
2023-04-19 14:08:27 +0530

    implement disease diagnosis page

--- app/src/main/java/com/example/coffeeplantdiseasedetection/HomeFragment.kt
+++ app/src/main/java/com/example/coffeeplantdiseasedetection/HomeFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
@@ -21,7 +24,12 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val button = view.findViewById<ImageView>(R.id.img_scan)
+        button.setOnClickListener {
+            val navController = Navigation.findNavController(view)
+            navController.navigate(R.id.action_homeFragment_to_diseaseDiagnosisFragment)
+        }
+        return view
     }
 }
