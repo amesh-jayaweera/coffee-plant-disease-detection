4a8ed056e74eb0807b9d3aefd00cea1074f05ff5
ameshmjayaweera <ameshmbjyw97@gmail.com>
2023-04-19 13:06:32 +0530

    implement home page

--- app/src/main/java/com/example/coffeeplantdiseasedetection/WelcomeFragment.kt
+++ app/src/main/java/com/example/coffeeplantdiseasedetection/WelcomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.Navigation
 
 /**
  * A simple [Fragment] subclass.
@@ -20,7 +22,12 @@ class WelcomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val button = view.findViewById<Button>(R.id.btn_start)
+        button.setOnClickListener {
+            val navController = Navigation.findNavController(view)
+            navController.navigate(R.id.action_welcomeFragment_to_homeFragment)
+        }
+        return view
     }
 }
